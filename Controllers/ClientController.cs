834e40dcabc71836fa3fad3b17f9a79853cae338
aya-sahlouf <104566069+aya-sahlouf@users.noreply.github.com>
2023-03-28 09:26:00 +0000

    display products

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,23 +1,47 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using couvre_plancher.Models;
+using couvre_plancher.Data;
 
 namespace couvre_plancher.Controllers;
 
 public class ClientController : Controller
 {
-    private readonly ILogger <ClientController> _logger;
+ private readonly ILogger<ClientController> _logger;
+    private readonly AplicationDbContext _db;
 
-    public ClientController(ILogger <ClientController> logger)
+    public ClientController(ILogger<ClientController> logger, AplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
-    {
-        return View();
+    { IEnumerable<CouvreplancherModel> couvre = _db.Couvreplancher;
+        return View(couvre);
     }
 
+//    [HttpGet]
+//  public IActionResult Index(int lar, int Long ,int idcouvre)
+//     {
+//         // double CalculSuperficie = lar*Long;
+//         // double CalculHTMateriaux =  12.9 * CalculSuperficie;
+//         // double CalculHTMainOeuvre= 20 * CalculSuperficie;
+//         // double CalculTaxeMateriaux= CalculHTMateriaux* 0.15;
+//         //  double CalculTaxeMainOeuvre= CalculHTMainOeuvre* 0.15;  
+//         //  double CalculTotalHT =CalculHTMateriaux+CalculHTMainOeuvre;
+//         //   double CalculTotalTTC = CalculTotalHT + CalculTaxeMainOeuvre +CalculTaxeMateriaux;
+       
+
+//         return View(facture);
+//     }
+
+   
+
+   
+
+ 
+    
     public IActionResult Validation()
     {
         return View();
